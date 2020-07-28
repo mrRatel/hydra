92831f650799765def6a9f947a0ccee0ed26df84
ratel <jxc19960306@outlook.com>
2020-07-28 23:30:40 +0800

    解决 cache 和spring事务的冲突

--- src/main/java/com/ratel/hydra/HydraApplication.java
+++ src/main/java/com/ratel/hydra/HydraApplication.java
@@ -5,11 +5,12 @@ import org.slf4j.MDC;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @MapperScan("com.ratel.hydra.**.mapper")
 @EnableConfigurationProperties
+@EnableTransactionManagement(order = 2000)
 public class HydraApplication {
 
     public static void main(String[] args) {
